1f977b45f30ace2268d815ba21c5b6ca4e0ac677
abhinavdangeti <abhinav@couchbase.com>
2014-11-05 14:18:00 -0800

    Modify Gets.java

--- src/Gets.java
+++ src/Gets.java
@@ -9,7 +9,7 @@ import com.couchbase.client.CouchbaseClient;
 public class Gets {
     public static void get_items (CouchbaseClient client, Variables V, String _prefix) {
         boolean checkFlag = true;
-        int count;
+        int count, attempts = 0;
         if (V.getSetRatio() == 0.0) {
             count = V.getItemCount() + V.getAddCount();
         } else {
@@ -19,6 +19,12 @@ public class Gets {
         while (checkFlag) {
             for (int i=0; i<(V.getItemCount() + V.getAddCount()); i++) {
                 if (count == 0) {
+                    System.out.println("All Gets Successful");
+                    checkFlag = false;
+                    break;
+                }
+                if (attempts == V.getItemCount() + V.getAddCount()) {
+                    System.out.println("Not all Gets successful");
                     checkFlag = false;
                     break;
                 }
@@ -26,6 +32,7 @@ public class Gets {
                 if (item != null) {
                     count --;
                 }
+                attempts++;
             }
         }
     }
